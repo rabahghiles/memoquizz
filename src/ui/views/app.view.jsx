06058e63b37131388dacb677d5c51eb9a7333c61
admin <admin@MacBook-Pro.local>
2023-02-21 22:37:19 +0100

    Design de la partie quizz

--- src/ui/views/app.view.jsx
+++ src/ui/views/app.view.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchCards } from '../../redux/actions/cards.actions';
 import { API } from "../../redux/constantes"
 import './styles/app.css';
+import '../components/styles/quizz.css';
 
 import Loader from "../components/loader.component";
 import CardsList from "../components/cardsList.component";
@@ -36,6 +37,23 @@ const App = () => {
         }
       </div>
       <FinalScore finalScore={finalScore} endGame={!game} />
+      <div className="quizz">
+        <div className="quizz-inner">
+          <div className="quizz-content">
+            <span className='quizz-question-number'>1/3</span>
+            <p className='quizz-question'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt fugit officia itaque animi adipisci cum, necessitatibus totam mollitia incidunt, quas magni eligendi illum dignissimos, cupiditate iusto. Cumque cum quos optio?</p>
+          </div>
+          <form className="quizz-form">
+            <div className="quizz-form-questions">
+              <label className="quiizz-form-checkbox">Choix 1<input type="checkbox" /></label>
+              <label className="quiizz-form-checkbox">Choix 2<input type="checkbox" /></label>
+              <label className="quiizz-form-checkbox">Choix 3<input type="checkbox" /></label>
+              <label className="quiizz-form-checkbox">Choix 4<input type="checkbox" /></label>
+            </div>
+            <button className="quizz-form-button">Confirmer</button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
